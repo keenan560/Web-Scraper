b158806b767ba69b71435ce9029617b9237b17d1
keenan560 <keenan560@gmail.com>
2019-08-29 23:00:26 -0400

    insalled packages

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const Port = 3000;
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(Port, () => {
+    console.log(`Listening on Port ${Port}`);
+})
